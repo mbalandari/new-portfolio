d72db09009bb0191e5232bebdb5a7dadc1d2ab91
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-01 14:08:38 +0300

    3rd

---
+++ src/Components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import avatar from '../img/avatar.png'
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <nav className="nav">
+                <div className="profile">
+                    <img src={avatar} alt="avatar" />
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/portfolio" exact activeClassName="active">
+                            Portfolio
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/blogs" exact activeClassName="active">
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+                <footer className="footer">
+                    <p>@2021 codingMaz</p>
+                </footer>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
