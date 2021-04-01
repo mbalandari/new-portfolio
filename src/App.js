9734865abd71df9cf0139e28534a62d438d140ac
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-01 18:43:52 +0300

    6th

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.scss';
+import { Switch, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar'
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import PortfolioPage from './Pages/PortfolioPage';
+import BlogsPage from './Pages/BlogsPage';
+import ContactPage from './Pages/ContactPage';
+
+
 
 function App() {
   return (
@@ -8,7 +16,25 @@ function App() {
         <NavBar />
       </div>
       <div className="main-content">
-
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+            <Route path="/portfolio" exact>
+              <PortfolioPage />
+            </Route>
+            <Route path="/blogs" exact>
+              <BlogsPage />
+            </Route>
+            <Route path="/contact" exact>
+              <ContactPage />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
   );
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { faTwitterSquare, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am <span>codingMaz.</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic quaerat aliquam fugiat deserunt totam nisi excepturi officiis dolorum similique magni nihil perferendis inventore ducimus, minima aliquid. Laudantium, culpa architecto!
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder"><FontAwesomeIcon icon={faTwitterSquare} className="icon tw" /></Link>
+                    <Link className="icon-holder"><FontAwesomeIcon icon={faGithub} className="icon gh" /></Link>
+                    <Link className="icon-holder"><FontAwesomeIcon icon={faLinkedin} className="icon li" /></Link>
+                </div>
+            </header>
         </div>
     )
 }
