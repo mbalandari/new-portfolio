56708becd2b8d1415fc89f891f0730fc02044df3
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-03 11:18:46 +0300

    Sat-1

---
+++ src/Components/SkillsSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SkillsSection = () => {
+    return (
+        <div className="SkillsSection">
+            <div className="skills-container">
+                <h5 className="skill-title"></h5>
+                <div className="skill-bar">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SkillsSection
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Title from '../Components/Title'
-import ImageSection from './ImageSection'
+import ImageSection from '../Components/ImageSection'
 
 const AboutPage = () => {
     return (
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'} />
             <ImageSection />
+            <Title title={'My Skills'} span={'My Skills'} />
         </div>
     )
 }
