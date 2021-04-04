a99423f4a0065623be0965f1b6a1574783e3b496
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-04 14:34:07 +0300

    Sun-5

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import allBlogs from '../Components/allBlogs'
+import Title from '../Components/Title'
 
 const BlogsPage = () => {
     return (
-        <div className="BlogsPage">
-            {
-                allBlogs.map((blog) => {
-                    return (
-                        <div className="blog" key={blog.id}>
-                            <div className="blog-content">
-                                <img src={blog.image} alt="pic" />
-                                <a href={blog.link} className="blog-link">
-                                    {blog.title}
-                                </a>
+        <div>
+            <div className="blog-title">
+                <Title title={'Recent Blogs'} span={'Recent Blogs'} />
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog) => {
+                        return (
+                            <div className="blog" key={blog.id}>
+                                <div className="blog-content">
+                                    <img src={blog.image} alt="pic" />
+                                    <a href={blog.link} className="blog-link">
+                                        {blog.title}
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
