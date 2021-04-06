6e36b330f28c510743ef5953c0d27d87b3adaf11
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-06 10:28:03 +0300

    Tue2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { Switch, Route } from 'react-router-dom'
+import { useState } from 'react';
 import NavBar from './Components/NavBar'
 import HomePage from './Pages/HomePage';
 import AboutPage from './Pages/AboutPage';
@@ -9,10 +10,21 @@ import ContactPage from './Pages/ContactPage';
 
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () => {
+    setNavToggle(!navToggle);
+  };
+
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${navToggle ? "nav-toggle" : ""}`}>
         <NavBar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="line-1"></div>
+        <div className="line-2"></div>
+        <div className="line-3"></div>
       </div>
       <div className="main-content">
         <div className="content">
